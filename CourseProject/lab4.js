4a3fc80c4372b8eaa4a008b91cf11d8cf3b6fa03
2YGitLeader <hitmwn595@gmail.com>
2026-04-06 22:17:12 +0200

    Added ascending sort by timestamp and ensured the first added item is extracted regardless of priority Finished lab4

--- CourseProject/lab4.js
+++ CourseProject/lab4.js
@@ -13,4 +13,9 @@ export class BiDirectionalPriorityQueue {
         return this.items.shift().item;
     }
 
-    
+    dequeueOldest() {
+        if (this.items.length === 0) return null;
+        this.items.sort((a, b) => a.timestamp - b.timestamp);
+        return this.items.shift().item;
+    }
+}
