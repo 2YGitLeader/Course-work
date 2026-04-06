adaeb0f333e329033eb202a9b8e1e0c72a21b639
2YGitLeader <hitmwn595@gmail.com>
2026-04-06 21:45:12 +0200

    Added enqueue method to BiDirectionalPriorityQueue and implemented object-based storage for items

--- CourseProject/lab4.js
+++ CourseProject/lab4.js
@@ -2,3 +2,7 @@ export class BiDirectionalPriorityQueue {
     constructor() {
         this.items = [];
     }
+    
+    enqueue(item, priority) {
+        this.items.push({ item, priority, timestamp: Date.now() });
+    }
