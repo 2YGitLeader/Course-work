fc11a4080b5b5a87f8cc8fc486c8164486d9a34e
2YGitLeader <hitmwn595@gmail.com>
2026-04-09 23:28:51 +0200

    Add loop through the array using a for-loop and check for (signal.aborted) status in each iteration Ensured efficient resource management by stopping execution early

--- CourseProject/lab5.js
+++ CourseProject/lab5.js
@@ -1,2 +1,7 @@
 export async function asyncMapPromise(array, asyncCallback, signal) {
     const results = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (signal?.aborted) {
+            throw new Error("The process has been cancelled");
+        }
