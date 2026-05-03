599e7b02d8b445f16d8679ba990dc2415666da5a
2YGitLeader <hitmwn595@gmail.com>
2026-05-03 20:36:38 +0200

    Added (for await...of) loop to handle asynchronous data chunks and implemented (processedCount) increment logic Later i add final return statement for the total count Completed lab 6

--- CourseProject/lab6.js
+++ CourseProject/lab6.js
@@ -1,2 +1,7 @@
 export async function processLargeDataStream(asyncIterable) {
     let processedCount = 0;
+    for await (const chunk of asyncIterable) {
+        processedCount++;
+    }
+    return processedCount;
+}
