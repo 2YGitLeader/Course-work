0756f38592702b6b2b25b113a5560b2f86b6812b
2YGitLeader <hitmwn595@gmail.com>
2026-05-13 18:38:57 +0200

    Implemented (subscribe) method using (eventBus.on) and (unsubscribe) method using (eventBus.off) Completed lab 7

--- CourseProject/lab7.js
+++ CourseProject/lab7.js
@@ -5,5 +5,14 @@ export const eventBus = new EventEmitter();
 export class Entity {
     constructor(name) {
         this.name = name;
-        this.handler = (msg) => console.log(`${this.name} отримав: ${msg}`);
+        this.handler = (msg) => console.log(`${this.name} received: ${msg}`);
     }
+
+    subscribe(event) {
+        eventBus.on(event, this.handler);
+    }
+    
+    unsubscribe(event) {
+        eventBus.off(event, this.handler);
+    }
+}
