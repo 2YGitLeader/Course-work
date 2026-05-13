a2bf0e62496886f2509eabcc104b40c4af051e6b
2YGitLeader <hitmwn595@gmail.com>
2026-05-13 13:30:22 +0200

    Defined (Entity) class for event bus participants Added constructor to initialize entity name and implemented (handler) arrow function to preserve (this) context

--- CourseProject/lab7.js
+++ CourseProject/lab7.js
@@ -1,3 +1,9 @@
 import { EventEmitter } from 'events';
 
 export const eventBus = new EventEmitter();
+
+export class Entity {
+    constructor(name) {
+        this.name = name;
+        this.handler = (msg) => console.log(`${this.name} отримав: ${msg}`);
+    }
