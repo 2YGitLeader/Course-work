243c66a7d53b1e570c97f2deb5fb7b499f7e2fd9
2YGitLeader <hitmwn595@gmail.com>
2026-05-16 15:28:21 +0200

    Added (fetch) method to intercept outgoing requests Implemented automatic injection of the Authorization heade and existing headers are preserved using object spreading

--- CourseProject/lab8.js
+++ CourseProject/lab8.js
@@ -2,3 +2,11 @@ export class AuthProxy {
     constructor(apiToken) {
         this.apiToken = apiToken;
     }
+
+    async fetch(url, options = {}) {
+        const modifiedOptions = {
+            ...options,
+            headers: {
+                ...options.headers,
+                'Authorization': `Bearer ${this.apiToken}`
+            }
