c42bc890a2997c67a9a718070378662080f02b59
2YGitLeader <hitmwn595@gmail.com>
2026-05-16 18:03:33 +0200

    Added request logging for better traceability and implemented actual fetch call using (globalThis.fetch) Completed lab8

--- CourseProject/lab8.js
+++ CourseProject/lab8.js
@@ -10,3 +10,8 @@ export class AuthProxy {
                 ...options.headers,
                 'Authorization': `Bearer ${this.apiToken}`
             }
+         };
+        console.log(`[Proxy] Sending a request to ${url}`);
+        return globalThis.fetch(url, modifiedOptions);
+    }
+}
